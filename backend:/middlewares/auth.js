e472a983abbb2eec8814a528188e23a53891aa33
Nikita Galushkin <gavrik_nikitka@mail.ru>
2020-12-18 09:13:08 +0300

    remove cookie

--- backend:/middlewares/auth.js
+++ backend:/middlewares/auth.js
@@ -18,7 +18,7 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    next(new UnauthorizedError('Необходима авторизация'));
+    throw new UnauthorizedError({ message: 'Необходима авторизация' });
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
