599377c72577ba55091a586e4c1e5f369fa64255
Nikita Galushkin <gavrik_nikitka@mail.ru>
2020-12-18 13:13:58 +0300

    set token now

--- backend:/controllers/users.js
+++ backend:/controllers/users.js
@@ -100,7 +100,7 @@ module.exports.login = (req, res, next) => {
         NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
         { expiresIn: '7d' },
       );
-      res.send({ token });
+      res.status(200).send({ token });
       // res
       //   .cookie('jwt', token, {
       //     maxAge: 3600000 * 24 * 7,
--- backend:/middlewares/auth.js
+++ backend:/middlewares/auth.js
@@ -3,33 +3,17 @@ const UnauthorizedError = require('../errors/UnauthorizedError');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
-// module.exports = (req, res, next) => {
-//   const { authorization } = req.headers;
-//   if (!authorization || !authorization.startsWith('Bearer ')) {
-//     throw new UnauthorizedError({ message: 'Необходима авторизация' });
-//   }
-//   const token = authorization.replace('Bearer ', '');
-//   let payload;
-//   try {
-//     payload = jwt.verify(token, `${NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret'}`);
-//   } catch(err) {
-//     throw new UnauthorizedError({ message: 'Необходима авторизация' });
-//   }
-//   req.user = payload;
-//   return next();
-// };
-
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UnauthorizedError({ message: 'Некорректно введён email или пароль' });
+    throw new UnauthorizedError({ message: 'Необходима авторизация' });
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    throw new UnauthorizedError({ message: 'Некорректно введён email или пароль' });
+    throw new UnauthorizedError({ message: 'Необходима авторизация' });
   }
   req.user = payload;
   next();
