78923b78b93f32b55ff2a3ebe1a541ff71380b9e
Nikita Galushkin <gavrik_nikitka@mail.ru>
2020-12-19 22:14:49 +0300

    add if

--- backend:/app.js
+++ backend:/app.js
@@ -47,27 +47,12 @@ app.post('/signup', validateUser, createUser);
 
 app.use('/', auth, usersRouter);
 app.use('/', auth, cardsRouter);
-// app.use('*', () => {
-//   throw new NotFoundError({ message:'Запрашиваемый ресурс не найден' });
-// });
-
-// app.use(errorLogger);
-
-// app.use(errors());
-
-// app.use((err, req, res, next) => {
-//   if (err.status) {
-//     res.status(err.status).send(err.message);
-//     return;
-//   }
-//   res.status(500).send({ message: `На сервере произошла ошибка: ${err.message}` });
-//   next();
-// });
 app.use(() => {
   throw new NotFoundError({ message:'Запрашиваемый ресурс не найден' });
 });
 
 app.use(errorLogger);
+
 app.use(errors());
 
 app.use((err, req, res, next) => {
@@ -75,14 +60,7 @@ app.use((err, req, res, next) => {
     res.status(err.status).send(err.message);
     return;
   }
-  const { statusCode = 500, message } = err;
-  res
-    .status(statusCode)
-    .send({
-      message: statusCode === 500
-        ? 'На сервере произошла ошибка'
-        : message,
-    });
+  res.status(500).send({ message: `На сервере произошла ошибка: ${err.message}` });
   next();
 });
 
