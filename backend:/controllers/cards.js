d228fa15f52bc9bdc53902ed71e9de8ad010974a
Nikita Galushkin <gavrik_nikitka@mail.ru>
2020-12-19 13:32:35 +0300

    add array

--- backend:/controllers/cards.js
+++ backend:/controllers/cards.js
@@ -14,7 +14,7 @@ module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
     .then((card) => {
-      res.send({ data: card });
+      res.send({ card });
     })
     .catch(() => {
       throw new BadRequestError({ message: 'Переданы некорректные данные' });
